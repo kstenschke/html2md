e13f6c671bffd8f889e4d2ed6567d5e1d4fb89a2
kstenschke <info@stenschke.com>
2020-10-12 00:20:20 +0200

    resolve circular complexity of too many conditions via strategy / factory pattern

--- html2md.hpp
+++ html2md.hpp
@@ -10,11 +10,144 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <map>
 
 namespace html2md {
 
+// Tag: base class for tag types
+struct Tag {
+  virtual u_int16_t OnHasLeftOpeningTag(
+      std::string *md, u_int16_t chars_in_current_line) = 0;
+
+  u_int16_t AppendToMd(std::string *md, char ch, u_int16_t chars_in_curr_line) {
+    *md += ch;
+
+    if (ch == '\n')
+      chars_in_curr_line = 0;
+    else
+      ++chars_in_curr_line;
+
+    return chars_in_curr_line;
+  }
+
+  u_int16_t AppendToMd(
+      std::string *md, const char *str, u_int16_t chars_in_curr_line) {
+    *md += str;
+
+    auto str_len = strlen(str);
+
+    for (int i = 0; i < str_len; ++i) {
+      if (str[i] == '\n')
+        chars_in_curr_line = 0;
+      else
+        ++chars_in_curr_line;
+    }
+
+    return chars_in_curr_line;
+  }
+};
+
+// Tag types for strategy pattern
+struct TagHeader2 : Tag {
+  u_int16_t OnHasLeftOpeningTag(
+      std::string *md, u_int16_t chars_in_current_line) override {
+    return AppendToMd(md, "\n\n\n### ", chars_in_current_line);
+  }
+};
+
+struct TagHeader3 : Tag {
+  u_int16_t OnHasLeftOpeningTag(
+      std::string *md, u_int16_t chars_in_current_line) override {
+    return AppendToMd(md, "\n\n\n#### ", chars_in_current_line);
+  }
+};
+
+struct TagHeader4 : Tag {
+  u_int16_t OnHasLeftOpeningTag(
+      std::string *md, u_int16_t chars_in_current_line) override {
+    return AppendToMd(md, "\n\n\n##### ", chars_in_current_line);
+  }
+};
+
+struct TagListItem : Tag {
+  u_int16_t OnHasLeftOpeningTag(
+      std::string *md, u_int16_t chars_in_current_line) override {
+    char prev_ch = (*md)[md->length() - 1];
+
+    if (prev_ch != '\n') AppendToMd(md, "\n", chars_in_current_line);
+
+    return AppendToMd(md, "* ", chars_in_current_line);;
+  }
+};
+
+struct TagUnorderedList : Tag {
+  u_int16_t OnHasLeftOpeningTag(
+      std::string *md, u_int16_t chars_in_current_line) override {
+    char prev_ch = (*md)[md->length() - 1];
+    char prev_prev_ch = (*md)[md->length() - 2];
+
+    if (prev_ch != '\n') AppendToMd(md, "\n", chars_in_current_line);
+
+    if (prev_prev_ch != '\n') AppendToMd(md, "\n", chars_in_current_line);
+
+    return 0;
+  }
+};
+
+struct TagOrderedList : Tag {
+  u_int16_t OnHasLeftOpeningTag(
+      std::string *md, u_int16_t chars_in_current_line) override {
+    char prev_ch = (*md)[md->length() - 1];
+    char prev_prev_ch = (*md)[md->length() - 2];
+
+    if (prev_ch != '\n') AppendToMd(md, "\n", chars_in_current_line);
+
+    if (prev_prev_ch != '\n') AppendToMd(md, "\n", chars_in_current_line);
+
+    return 0;
+  }
+};
+
+struct TagBold : Tag {
+  u_int16_t OnHasLeftOpeningTag(
+      std::string *md, u_int16_t chars_in_current_line) override {
+    char prev_ch = (*md)[md->length() - 1];
+
+    if (prev_ch != ' ')
+      chars_in_current_line = AppendToMd(md, ' ', chars_in_current_line);
+
+    return AppendToMd(md, "**", chars_in_current_line);
+  }
+};
+
+struct TagDiv : Tag {
+  u_int16_t OnHasLeftOpeningTag(
+      std::string *md, u_int16_t chars_in_current_line) override {
+    char prev_ch = (*md)[md->length() - 1];
+    char prev_prev_ch = (*md)[md->length() - 2];
+
+    if (prev_ch != '\n') AppendToMd(md, '\n', chars_in_current_line);
+
+    if (prev_prev_ch != '\n') AppendToMd(md, '\n', chars_in_current_line);
+
+    return 0;
+  }
+};
+
+// Main class: HTML to Markdown converter
 class Converter {
  public:
+  ~Converter() {
+//    delete tags_[kTagDiv];
+//    delete tags_[kTagHeader2];
+//    delete tags_[kTagHeader3];
+//    delete tags_[kTagHeader4];
+//    delete tags_[kTagListItem];
+//    delete tags_[kTagUnorderedList];
+//    delete tags_[kTagOrderedList];
+//    delete tags_[kTagBold];  // same as kTagStrong
+  }
+
   static std::string Convert(std::string *html) {
     auto *instance = new Converter();
 
@@ -54,6 +187,28 @@ class Converter {
 
   const std::string &GetMd_() const {
     return md_;
+  }
+
+  void AppendToMd(char ch) {
+    md_ += ch;
+
+    if (ch == '\n')
+      chars_in_curr_line_ = 0;
+    else
+      ++chars_in_curr_line_;
+  }
+
+  void AppendToMd(const char *str) {
+    md_ += str;
+
+    auto str_len = strlen(str);
+
+    for (int i = 0; i < str_len; ++i) {
+      if (str[i] == '\n')
+        chars_in_curr_line_ = 0;
+      else
+        ++chars_in_curr_line_;
+    }
   }
 
  private:
@@ -105,7 +260,21 @@ class Converter {
   std::string md_;
   size_t md_len_;
 
-  Converter() = default;
+  std::map<std::string, Tag*> tags_;
+
+  Converter() {
+    tags_[kTagHeader2] = new TagHeader2();
+    tags_[kTagHeader3] = new TagHeader3();
+    tags_[kTagHeader4] = new TagHeader4();
+    tags_[kTagListItem] = new TagListItem();
+    tags_[kTagUnorderedList] = new TagUnorderedList();
+    tags_[kTagOrderedList] = new TagOrderedList();
+    tags_[kTagDiv] = new TagDiv();
+
+    TagBold *bold = new TagBold();
+    tags_[kTagBold] = bold;
+    tags_[kTagStrong] = bold;
+  }
 
   // Trim from start (in place)
   static void LTrim(std::string *s) {
@@ -312,30 +481,16 @@ class Converter {
     return true;
   }
 
+  // '>' = has left opening-tag
   void OnHasLeftOpeningTag() {
-    // '>' = has left opening-tag
-    if (current_tag_ == kTagBold || current_tag_ == kTagStrong) {
-      if (prev_ch_ != ' ') AppendToMd(' ');
+    Tag* tag = tags_[current_tag_];
 
-      AppendToMd("**");
-      ++chars_in_curr_line_;
-    } else if (current_tag_ == kTagHeader2) {
-      AppendToMd("\n\n\n### ");
-    } else if (current_tag_ == kTagHeader3) {
-      AppendToMd("\n\n\n#### ");
-    } else if (current_tag_ == kTagHeader4) {
-      AppendToMd("\n\n\n##### ");
-    } else if (current_tag_ == kTagListItem) {
-      if (prev_ch_ != '\n') AppendToMd('\n');
+    if (tag != nullptr)
+      chars_in_curr_line_ = tag->OnHasLeftOpeningTag(&md_, chars_in_curr_line_);
 
-      AppendToMd("* ");
-    } else if (current_tag_ == kTagUnorderedList
-        || current_tag_ == kTagOrderedList
-        || current_tag_ == kTagDiv) {
-      if (prev_ch_ != '\n') AppendToMd('\n');
-
-      if (prev_prev_ch_ != '\n') AppendToMd('\n');
-    } else if (current_tag_ == kTagNoScript) {
+    // TODO(kay): forward declare Converter and pass it into factory tag
+    // TODO(kay): than add tag types for the following
+    if (current_tag_ == kTagNoScript) {
       is_in_child_of_noscript_tag_ = true;
     } else if (current_tag_ == kTagScript) {
       is_in_script_tag_ = true;
@@ -346,31 +501,11 @@ class Converter {
     }
   }
 
-  void AppendToMd(char ch) {
-    md_ += ch;
-
-    if (ch == '\n')
-      chars_in_curr_line_ = 0;
-    else
-      ++chars_in_curr_line_;
-  }
-
-  void AppendToMd(const char *str) {
-    md_ += str;
-
-    auto str_len = strlen(str);
-
-    for (int i = 0; i < str_len; ++i) {
-      if (str[i] == '\n')
-        chars_in_curr_line_ = 0;
-      else
-        ++chars_in_curr_line_;
-    }
-  }
-
   void OnHasLeftClosingTag() {
     // '>' = has left closing-tag
     is_closing_tag_ = false;
+
+    // TODO(kay): replace  multifold conditions by factory / strategy pattern
 
     if (current_tag_ == kTagAnchor) {
 //        md_ += '[';
@@ -418,7 +553,8 @@ class Converter {
         || is_in_template_tag_
         || current_tag_ == kTagLink
         || current_tag_ == kTagMeta
-        || current_tag_ == kTagScript)
+        || current_tag_ == kTagScript
+        || ch == '\n')
       return true;
 
     prev_ch_ = md_len_ > 0 ? md_[md_len_ - 1] : '.';
@@ -431,8 +567,6 @@ class Converter {
           || prev_ch_ == '\n')
         return true;
     }
-
-    if (ch == '\n') return true;
 
     if (ch == '.' && prev_ch_ == ' ') {
       ShortenMarkdown();
@@ -451,6 +585,7 @@ class Converter {
   }
 };  // Converter
 
+// Static wrapper for simple invocation
 std::string Convert(std::string html) {
   return html2md::Converter::Convert(&html);
 }
